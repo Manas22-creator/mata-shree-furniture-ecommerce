4e17496b77e73c57b41b35c422b7eae96fc2c5ed
Manas Pandey <pandeymanas220204@gmail.com>
2026-01-04 15:27:34 +0530

    Fix: Improve footer responsive design on mobile devices - prevent overflow and ensure categories display properly

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -14,9 +14,15 @@ const Footer = () => {
                         </Link>
                         <p>Crafting beautiful furniture for over 15 years. We believe in quality, craftsmanship, and creating spaces that reflect your personality.</p>
                         <div className="social-links">
-                            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><img src="/assets/icons/facebook.svg" alt="Facebook" /></a>
-                            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><img src="/assets/icons/instagram.svg" alt="Instagram" /></a>
-                            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><img src="/assets/icons/twitter.svg" alt="Twitter" /></a>
+                            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                                <img src="/assets/icons/facebook.svg" alt="Facebook" />
+                            </a>
+                            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                                <img src="/assets/icons/instagram.svg" alt="Instagram" />
+                            </a>
+                            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+                                <img src="/assets/icons/twitter.svg" alt="Twitter" />
+                            </a>
                         </div>
                     </div>
                     <div className="footer-links">
@@ -40,9 +46,18 @@ const Footer = () => {
                     <div className="footer-contact">
                         <h4>Contact Info</h4>
                         <ul>
-                            <li><img src="/assets/icons/phone-white.svg" alt="Phone" /> +91 98765 43210</li>
-                            <li><img src="/assets/icons/email-white.svg" alt="Email" /> info@matashreefurniture.com</li>
-                            <li><img src="/assets/icons/location-white.svg" alt="Address" /> 123 Furniture Street, Design District</li>
+                            <li>
+                                <img src="/assets/icons/phone-white.svg" alt="Phone" />
+                                <span>+91 98765 43210</span>
+                            </li>
+                            <li>
+                                <img src="/assets/icons/email-white.svg" alt="Email" />
+                                <span>info@matashreefurniture.com</span>
+                            </li>
+                            <li>
+                                <img src="/assets/icons/location-white.svg" alt="Address" />
+                                <span>123 Furniture Street, Design District</span>
+                            </li>
                         </ul>
                     </div>
                 </div>
